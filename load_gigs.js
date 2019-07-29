3cb699494b8b99861feb87fa99bcbf45a612e690
Tyler D <tyler@mishmash.dev>
2019-07-29 18:37:11 -0400

    working

---
+++ load_gigs.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+
+(async () => {
+  const gigUrlFile = "gig_urls.txt";
+  const searchRe = /developer|web|site|javascript|php|coding|computer/;
+  const possibleGigs = [];
+
+  //load the gig_urls.txt
+  try {
+    const gigUrls = fs.readFileSync(gigUrlFile, "utf8");
+    const urls = gigUrls.split("\n");
+
+    //open the url
+
+    for (let i = 0; i < urls.length; i++) {
+      const url = urls[i];
+      console.log(`Finding gigs in ${url}`);
+      const response = await axios.get(url);
+      const $ = cheerio.load(response.data);
+      // find a.result-title
+
+      const gigs = $("a.result-title");
+
+      gigs.each((index, gig) => {
+        const gigText = $(gig).html();
+        const gigUrl = $(gig).attr("href");
+
+        if (gigText.search(searchRe) !== -1) {
+          const possibleGig = `${gigText} : ${gigUrl}`;
+          possibleGigs.push(`${gigText} : ${gigUrl}`);
+        }
+      });
+    }
+    if (possibleGigs.length !== 0) {
+      console.log(`Found ${possibleGigs.length} possible gigs`);
+      writeGigs(possibleGigs);
+    } else {
+      console.log("No Gigs to write?");
+    }
+  } catch (err) {
+    console.log(err.message);
+    if (possibleGigs.length !== 0) {
+      writeGigs(possibleGigs);
+    } else {
+      console.log("No Gigs to write?");
+    }
+  }
+
+  function writeGigs(gigData) {
+    fs.writeFileSync("possible_gigs.txt", gigData.join("\n"));
+  }
+})();
